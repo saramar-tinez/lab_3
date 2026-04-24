018052d7f8dd1cf2c34a741ce582e5f7d20e7579
saramar-tinez <sara.martinezu@udea.edu.co>
2026-04-23 20:02:00 -0500

    subiendo proyecto qt

---
+++ build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_lab_3_FILE C:/INFORMATICA_2/Laboratorio3/lab_3/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/lab_3.exe)
+set(__QT_DEPLOY_TARGET_lab_3_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_lab_3_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
